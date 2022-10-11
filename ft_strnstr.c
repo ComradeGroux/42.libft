ba5536ce290d8596ca9333d806198693830b9abb
vgroux <vgroux@student.42lausanne.ch>
2022-10-11 19:15:42 +0200

    correction strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:25:08 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/11 13:39:08 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/11 19:14:51 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,22 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	int		validate;
 
-	i = 0;
-	validate = 0;
 	if (needle[0] == '\0')
-		return (haystack);
-	while (i < len)
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j]
-			&& needle[j] != '\0' && haystack[i + j] != '\0')
-			j++;
-		if (needle[j] == '\0')
-			return ((char *)haystack[i]);
+		if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] == needle[j] && i + j < len)
+			{
+				j++;
+				if (needle[j] == '\0')
+					return ((char *)&haystack[i]);
+			}
+		}
 		i++;
 	}
 	return (NULL);
