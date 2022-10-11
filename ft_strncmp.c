3fe229fce6b84621ee153498d215dfc8eb3c946c
vgroux <vgroux@student.42lausanne.ch>
2022-10-11 19:25:40 +0200

    correction strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:34:41 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/11 13:13:01 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/11 19:24:58 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
