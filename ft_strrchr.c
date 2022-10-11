719f658d153750e96907f067351702a6fe9a3fb6
vgroux <vgroux@student.42lausanne.ch>
2022-10-11 19:22:48 +0200

    correction strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:17:34 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/10 14:45:23 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/11 19:21:36 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_strrchr(const char *s, int c)
 
 	i = ft_strlen(s);
 	ptr_temp = (char *)s;
-	while (s[i] != c && i != 0)
+	while (ptr_temp[i] != (char)c && i != 0)
 		i--;
-	if (s[i] != c)
+	if (ptr_temp[i] != (char)c)
 		return (0);
 	else
 		return (&ptr_temp[i]);
