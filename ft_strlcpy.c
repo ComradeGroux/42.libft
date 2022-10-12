dc16dbe8794e8d9ef2b53dce9fbc440c9c104577
vgroux <vgroux@student.42lausanne.ch>
2022-10-12 21:20:49 +0200

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:40:41 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/11 15:27:23 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/12 21:20:17 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	while (dst[i] != '\0' && src[i] != '\0' && i < dstsize)
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (src[i] != '\0' && i < (dstsize - 1))
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	if (dst[i] != '\0' && dstsize != 0)
-		dst[i] = '\0';
+	dst[i] = '\0';
 	return (ft_strlen(src));
 }
