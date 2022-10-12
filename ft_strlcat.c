15d2feca42c7d2ceba6acc2309645efb389f1176
vgroux <vgroux@student.42lausanne.ch>
2022-10-12 17:48:29 +0200

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:26:05 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/11 15:32:39 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/12 17:35:13 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
-	size_t	temp;
+	size_t	res;
 
 	i = ft_strlen(dst);
 	j = 0;
-	temp = ft_strlen(src) + ft_strlen(dst);
-	while (src[j] != '\0' && i < dstsize - 1)
+	if (dstsize > i)
+		res = ft_strlen((char *)src) + i;
+	else
+		res = ft_strlen((char *)src) + dstsize;
+	while (src[j] != '\0' && i + 1 < dstsize)
 	{
 		dst[i] = src[j];
 		i++;
 		j++;
 	}
-	return (temp);
+	dst[i] = '\0';
+	return (res);
 }
