e85d0f32ec7be667dd2b907d033669725980f77a
vgroux <vgroux@student.42lausanne.ch>
2022-10-13 17:45:18 +0200

    modif calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 18:42:01 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/11 16:49:31 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/13 17:43:19 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
-	size_t	i;
 
-	i = 0;
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
