9e74d356f9867f825dd8cc3d782f93e616206a04
vgroux <vgroux@student.42lausanne.ch>
2022-10-14 21:24:25 +0200

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 19:53:44 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/14 21:15:25 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/14 21:23:26 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ static int	ft_intlen(long n)
 	if (n < 0)
 	{
 		n *= -1;
-		len = 1;
+		len++;
 	}
+	else if (n == 0)
+		len++;
 	while (n > 0)
 	{
 		n /= 10;
@@ -33,15 +35,26 @@ static int	ft_intlen(long n)
 char	*ft_itoa(int n)
 {
 	char	*str;
-	char	sign;
 	long	num;
 	size_t	i;
 
 	num = n;
-	i = ft_intlen(n);
+	i = ft_intlen(num);
 	str = (char *)malloc((i + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
+	if (num < 0)
+	{
+		num *= -1;
+		str[0] = '-';
+	}
 	str[i--] = '\0';
-	
+	if (num == 0)
+		str[i] = '0';
+	while (num > 0)
+	{
+		str[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+	return (str);
 }
