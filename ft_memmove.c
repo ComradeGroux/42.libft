2ceca47743219722dc5cf5b0e8ee20e673a42e16
vgroux <vgroux@student.42lausanne.ch>
2022-10-18 13:11:30 +0200

    modif memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,19 +6,12 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:57:15 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/14 17:56:39 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/18 13:10:04 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-	ligns 29 & 31:
-		comparison between the adresses point by the pointers
-			if 	 ==		->	the pointers point to the same adress
-			if	 <		->	the first point to an adress that is
-							higher in the stack that the second one
-*/
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*temp;
