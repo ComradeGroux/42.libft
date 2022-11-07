be77cb780e5f6777b368aa4dd5182083b2d11dfc
Victor Groux <victorgroux99@gmail.com>
2022-11-07 17:22:27 +0100

    suppression fonctions interdites

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 12:20:07 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/31 18:41:09 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/11/07 17:21:43 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,4 +86,5 @@ int		ft_gnl_strchr(char *str);
 char	*ft_gnl_strjoin(char *line, char *buff);
 char	*ft_gnl_strndup(char *str, size_t end);
 char	*ft_gnl_remove_free(char *buffer);
+
 #endif
